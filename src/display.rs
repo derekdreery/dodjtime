df99594e08a4ce3d994c93f936089cea73fb0155
Richard Dodd <richard.o.dodd@gmail.com>
2021-03-20 14:51:47 +0000

    Save commit before trying out borrowing branch.

--- src/display.rs
+++ src/display.rs
@@ -1,42 +1,63 @@
-use core::mem;
-use nrf52832_hal::{
-    gpio::{self, p0::Parts, Disconnected, Level, Output, Pin, PushPull},
-    pac,
-    spim::{self, Spim},
-    Delay,
+use core::{
+    mem::{self, MaybeUninit},
+    pin::Pin as StdPin,
+    ptr,
 };
-
-use crate::PushPullPin;
-
-/// Object that logically owns and manages a ST7789 display.
-pub enum Display {
-    PowerOn {
-        /// backlight
-        bl_pin: PushPullPin,
-        /// TODO work out what this is.
-        cs_pin: PushPullPin,
-        //display: ST7789<SPIInterfaceNoCS<Spim<pac::SPIM0>, PushPullPin>, PushPullPin>,
+use embassy::{
+    time::{Duration, Timer},
+    traits::spi::FullDuplex,
+};
+use embassy_nrf::{
+    hal::{
+        gpio::{Disconnected, Level, Output, Pin, PushPull},
+        prelude::*,
     },
-    PowerOff {
-        /// backlight
-        bl_pin: Pin<Disconnected>,
-        /// reset
-        rst_pin: Pin<Disconnected>,
-        /// Chip select pin: hold low when using the display adaptor.
-        cs_pin: Pin<Disconnected>,
-        /// data/clock switch
-        dc_pin: Pin<Disconnected>,
-        /// SPI clock to LCD
-        spi_clk_pin: Pin<Disconnected>,
-        /// SPI master-out-slave-in to LCD
-        spi_mosi_pin: Pin<Disconnected>,
-    },
-    /// Used so we can take all the contents of `Display`, do stuff with them, then put them back,
-    /// all the while having a valid value.
-    Dummy,
+    spim::{self, Spim},
+};
+use pin_utils::{pin_mut, unsafe_pinned, unsafe_unpinned};
+use rtt_target::rprintln;
+
+const DISPLAY_BATCH_SIZE: usize = 2048;
+
+pub struct DisplayOn<S: spim::Instance> {
+    /// Backlight pin
+    bl_pin: Pin<Output<PushPull>>,
+    /// Reset pin
+    rst_pin: Pin<Output<PushPull>>,
+    /// Chip select pin
+    cs_pin: Pin<Output<PushPull>>,
+    /// data/clock switch
+    dc_pin: Pin<Output<PushPull>>,
+    /// SPI master for controlling display
+    spim: Spim<S>,
+    /// Pin numbers to recover SPI pins on power off.
+    spi_clk_psel_bits: u32,
+    spi_mosi_psel_bits: u32,
 }
 
-impl Display {
+pub struct DisplayOff<S: spim::Instance> {
+    /// backlight
+    bl_pin: Pin<Disconnected>,
+    /// reset
+    rst_pin: Pin<Disconnected>,
+    /// Chip select pin: hold low when using the display adaptor.
+    cs_pin: Pin<Disconnected>,
+    /// data/clock switch
+    dc_pin: Pin<Disconnected>,
+    /// SPI clock to LCD
+    spi_clk_pin: Pin<Disconnected>,
+    /// SPI master-out-slave-in to LCD
+    spi_mosi_pin: Pin<Disconnected>,
+    /// SPI master peripheral
+    spim_p: S,
+    /// Matching SPI interrupt
+    interrupt: S::Interrupt,
+}
+
+impl<S> DisplayOff<S>
+where
+    S: spim::Instance,
+{
     /// Create the display from its constituent parts.
     ///
     /// Does not do any I/O.
@@ -47,48 +68,346 @@ impl Display {
         dc_pin: Pin<Disconnected>,
         spi_clk_pin: Pin<Disconnected>,
         spi_mosi_pin: Pin<Disconnected>,
+        spim_p: S,
+        interrupt: S::Interrupt,
     ) -> Self {
-        Self::PowerOff {
+        Self {
             bl_pin,
             rst_pin,
             cs_pin,
             dc_pin,
             spi_clk_pin,
             spi_mosi_pin,
+            spim_p,
+            interrupt,
         }
     }
 
     /// Assert that the display is powered on
-    pub async fn power_on(&mut self) {
-        if matches!(self, Self::PowerOff { .. }) {
-            // Take the contents of `Display` out to manipulate them.
-            let this = mem::replace(self, Display::Dummy);
+    pub async fn power_on(self) -> DisplayOn<S> {
+        // Turn on pins
+        let spi_clk_psel_bits = self.spi_clk_pin.psel_bits();
+        let spi_mosi_psel_bits = self.spi_mosi_pin.psel_bits();
+        let bl_pin = self.bl_pin.into_push_pull_output(Level::Low);
+        let rst_pin = self.rst_pin.into_push_pull_output(Level::Low);
+        let cs_pin = self.cs_pin.into_push_pull_output(Level::Low);
+        let dc_pin = self.dc_pin.into_push_pull_output(Level::Low);
+        let spi_clk_pin = self.spi_clk_pin.into_push_pull_output(Level::Low);
+        let spi_mosi_pin = self.spi_mosi_pin.into_push_pull_output(Level::Low);
 
-            if let Self::PowerOff {
-                bl_pin,
-                rst_pin,
-                cs_pin,
-                dc_pin,
-                spi_clk_pin,
-                spi_mosi_pin,
-            } = this
-            {
-                // Turn on pins
-                let bl_pin = bl_pin.into_push_pull_output(Level::Low);
-                let rst_pin = rst_pin.into_push_pull_output(Level::Low);
-                let cs_pin = cs_pin.into_push_pull_output(Level::Low);
-                let dc_pin = dc_pin.into_push_pull_output(Level::Low);
-                let spi_clk_pin = spi_clk_pin.into_push_pull_output(Level::Low);
-                let spi_mosi_pin = spi_mosi_pin.into_push_pull_output(Level::Low);
-                todo!()
-            } else {
-                // Safety: we just checked the variant.
-                unsafe { core::hint::unreachable_unchecked() }
+        // construct spim
+        let spi_config = spim::Config {
+            pins: spim::Pins {
+                sck: spi_clk_pin,
+                mosi: Some(spi_mosi_pin),
+                miso: None,
+            },
+            frequency: spim::Frequency::M8,
+            mode: spim::MODE_3,
+            orc: 122,
+        };
+        let spim = Spim::new(self.spim_p, self.interrupt, spi_config);
+
+        let mut display = DisplayOn {
+            bl_pin,
+            rst_pin,
+            cs_pin,
+            dc_pin,
+            spim,
+            spi_clk_psel_bits,
+            spi_mosi_psel_bits,
+        };
+
+        // hard reset the display
+        display.hard_reset().await;
+
+        unsafe {
+            let mut display = StdPin::new_unchecked(&mut display);
+            display.send_command(Instruction::SWRESET).await; // reset display
+            Timer::after(Duration::from_millis(150)).await;
+            display.send_command(Instruction::SLPOUT).await; // turn off sleep
+            Timer::after(Duration::from_millis(10)).await;
+            display.send_command(Instruction::INVOFF).await; // turn off invert
+            display.send_command(Instruction::VSCRDER).await; // vertical scroll definition
+            let data = [0u8, 0u8, 0x14u8, 0u8, 0u8, 0u8];
+            display.send_data(&data).await; // 0 TSA, 320 VSA, 0 BSA
+            display.send_command(Instruction::MADCTL).await; // left -> right, bottom -> top RGB
+            let data = [0b0000_0000];
+            display.send_data(&data).await;
+            display.send_command(Instruction::COLMOD).await; // 16bit 65k colors
+            let data = [0b0101_0101];
+            display.send_data(&data).await;
+            display.send_command(Instruction::INVON).await; // hack?
+            Timer::after(Duration::from_millis(10)).await;
+            display.send_command(Instruction::NORON).await; // turn on display
+            Timer::after(Duration::from_millis(10)).await;
+            display.send_command(Instruction::DISPON).await; // turn on display
+            Timer::after(Duration::from_millis(10)).await;
+        } // pinned ref is dropped, releasing display
+
+        display
+    }
+}
+
+impl<S> DisplayOn<S>
+where
+    S: spim::Instance,
+{
+    unsafe_pinned!(spim: Spim<S>);
+    unsafe_unpinned!(bl_pin: Pin<Output<PushPull>>);
+    unsafe_unpinned!(rst_pin: Pin<Output<PushPull>>);
+    unsafe_unpinned!(cs_pin: Pin<Output<PushPull>>);
+    unsafe_unpinned!(dc_pin: Pin<Output<PushPull>>);
+
+    pub async fn power_off(self) -> DisplayOff<S> {
+        let (spim_p, interrupt) = self.spim.into_inner();
+        // the pins are now unused, so safe to steal them again. TODO maybe suggest that
+        // `free` should return ownership of the pins
+        let (spi_clk_pin, spi_mosi_pin): (Pin<Output<PushPull>>, Pin<Output<PushPull>>) = unsafe {
+            (
+                Pin::from_psel_bits(self.spi_clk_psel_bits),
+                Pin::from_psel_bits(self.spi_mosi_psel_bits),
+            )
+        };
+        DisplayOff {
+            bl_pin: self.bl_pin.into_disconnected(),
+            rst_pin: self.rst_pin.into_disconnected(),
+            cs_pin: self.cs_pin.into_disconnected(),
+            dc_pin: self.dc_pin.into_disconnected(),
+            spi_clk_pin: spi_clk_pin.into_disconnected(),
+            spi_mosi_pin: spi_mosi_pin.into_disconnected(),
+            spim_p,
+            interrupt,
+        }
+    }
+
+    /// Send a display command.
+    ///
+    /// There will be a different function for each N used. Might be better to push the length to
+    /// runtime with a slice, but then would have to work out how to provide space for discard.
+    /// alloca would be perfect.
+    async fn send_commands(self: &mut StdPin<&mut Self>, instructions: &[Instruction]) {
+        self.as_mut().dc_pin().set_low().unwrap();
+        // Safety: instructions are valid u8.
+        self.as_mut()
+            .spim()
+            .write(unsafe { mem::transmute(&[instructions][..]) })
+            .await
+            .unwrap()
+    }
+
+    #[inline]
+    async fn send_command(self: &mut StdPin<&mut Self>, instruction: Instruction) {
+        self.as_mut().dc_pin().set_low().unwrap();
+        self.send_commands(&[instruction]).await
+    }
+
+    /// Send display data.
+    ///
+    /// Panics if display not in right mode.
+    async fn send_data(self: &mut StdPin<&mut Self>, data: &[u8]) {
+        self.as_mut().dc_pin().set_high().unwrap();
+        self.as_mut().spim().write(data).await.unwrap()
+    }
+
+    pub async fn set_pixel(self: &mut StdPin<&mut Self>, x: u16, y: u16, color: u16) {
+        self.set_address_window(x, y, x, y).await;
+        self.send_command(Instruction::RAMWR).await;
+        self.send_data(&color.to_be_bytes()).await;
+    }
+
+    /// Set a region of pixels from an iterator. Pixels are chunked into DISPLAY_BATCH_SIZE
+    /// batches, the size of which is a trade-off between space and speed.
+    pub async fn set_pixel_region(
+        self: &mut StdPin<&mut Self>,
+        x0: u16,
+        y0: u16,
+        x1: u16,
+        y1: u16,
+        pixels: impl Iterator<Item = u16>,
+    ) {
+        unsafe {
+            self.set_address_window(x0, y0, x1, y1).await;
+            self.send_command(Instruction::RAMWR).await;
+            let mut chunks = Chunks::new(FlattenArray::new(pixels.map(|p| p.to_be_bytes())));
+            while let Some(buf) = chunks.next() {
+                self.send_data(buf).await;
             }
         }
     }
 
-    fn power_off(&mut self) {
-        todo!()
+    async fn set_address_window(
+        self: &mut StdPin<&mut Self>,
+        x_start: u16,
+        y_start: u16,
+        x_end: u16,
+        y_end: u16,
+    ) {
+        rprintln!("set address window");
+        let x_start = x_start.to_be_bytes();
+        let x_end = x_end.to_be_bytes();
+        let x = [x_start[0], x_start[1], x_end[0], x_end[1]];
+        rprintln!("send command CASET");
+        self.send_command(Instruction::CASET).await;
+        rprintln!("send data {:?}", x);
+        self.send_data(&x).await;
+        let y_start = y_start.to_be_bytes();
+        let y_end = y_end.to_be_bytes();
+        let y = [y_start[0], y_start[1], y_end[0], y_end[1]];
+        rprintln!("send command RASET");
+        self.send_command(Instruction::RASET).await;
+        rprintln!("send data {:?}", y);
+        self.send_data(&y).await;
+        rprintln!("finish: set address window");
+    }
+
+    /// Reset the screen.
+    pub async fn hard_reset(&mut self) {
+        hard_reset(&mut self.rst_pin).await
+    }
+}
+
+async fn hard_reset(rst_pin: &mut Pin<Output<PushPull>>) {
+    rst_pin.set_high().unwrap();
+    Timer::after(Duration::from_micros(10)).await;
+    rst_pin.set_low().unwrap();
+    Timer::after(Duration::from_micros(10)).await;
+    rst_pin.set_high().unwrap();
+    Timer::after(Duration::from_micros(10)).await;
+}
+
+/// ST7789 instructions.
+#[repr(u8)]
+enum Instruction {
+    NOP = 0x00,
+    SWRESET = 0x01,
+    RDDID = 0x04,
+    RDDST = 0x09,
+    SLPIN = 0x10,
+    SLPOUT = 0x11,
+    PTLON = 0x12,
+    NORON = 0x13,
+    INVOFF = 0x20,
+    INVON = 0x21,
+    DISPOFF = 0x28,
+    DISPON = 0x29,
+    CASET = 0x2A,
+    RASET = 0x2B,
+    RAMWR = 0x2C,
+    RAMRD = 0x2E,
+    PTLAR = 0x30,
+    VSCRDER = 0x33,
+    COLMOD = 0x3A,
+    MADCTL = 0x36,
+    VSCAD = 0x37,
+    VCMOFSET = 0xC5,
+}
+
+/// Collects an iterator into batches for DMA.
+struct Chunks<I> {
+    iter: I,
+    buf: [MaybeUninit<u8>; DISPLAY_BATCH_SIZE],
+}
+
+impl<I> Chunks<I>
+where
+    I: Iterator<Item = u8>,
+{
+    fn new(iter: I) -> Self {
+        Chunks {
+            iter,
+            buf: MaybeUninit::uninit_array(),
+        }
+    }
+
+    // Can't use iterator because we can't tie the lifetime of the array to self.
+    fn next(&mut self) -> Option<&[u8]> {
+        // maybe there is a better way to do this than a for loop
+        let mut i = 0;
+        while i < DISPLAY_BATCH_SIZE {
+            match self.iter.next() {
+                Some(b) => {
+                    self.buf[i].write(b);
+                    i += 1;
+                }
+                None => break,
+            }
+        }
+        if i > 0 {
+            unsafe { Some(MaybeUninit::slice_assume_init_ref(&self.buf[..i])) }
+        } else {
+            None
+        }
+    }
+}
+
+/// Consumes an iterator of arrays, and returns an iterator flatting the arrays into a single
+/// stream of values.
+struct FlattenArray<T, I, const N: usize> {
+    inner: I,
+    buf: [MaybeUninit<T>; N],
+    pos: usize,
+}
+
+impl<T, I, const N: usize> FlattenArray<T, I, { N }>
+where
+    I: Iterator<Item = [T; N]>,
+{
+    fn new(inner: I) -> Self {
+        Self {
+            inner,
+            buf: MaybeUninit::uninit_array(),
+            pos: 0,
+        }
+    }
+}
+
+impl<T, I, const N: usize> Iterator for FlattenArray<T, I, { N }>
+where
+    I: Iterator<Item = [T; N]>,
+{
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        unsafe {
+            if self.pos < N {
+                // we've still got data
+                let out = Some(self.buf[self.pos].assume_init_read());
+                self.pos += 1;
+                out
+            } else {
+                // get some more data
+                match self.inner.next() {
+                    Some(arr) => {
+                        // TODO better way of copying array? mem::copy_nonoverlapping?
+                        let arr = MaybeUninit::new(arr);
+                        ptr::copy_nonoverlapping(
+                            &arr as *const _ as *const [MaybeUninit<T>; N],
+                            &mut self.buf as *mut [MaybeUninit<T>; N],
+                            1,
+                        );
+                        let out = Some(self.buf[0].assume_init_read());
+                        self.pos = 1;
+                        out
+                    }
+                    None => None,
+                }
+            }
+        }
+    }
+}
+
+impl<T, I, const N: usize> core::iter::FusedIterator for FlattenArray<T, I, { N }> where
+    I: Iterator<Item = [T; N]> + core::iter::FusedIterator
+{
+}
+
+// We need to implement drop because only we know how many of buf are live.
+impl<T, I, const N: usize> Drop for FlattenArray<T, I, { N }> {
+    fn drop(&mut self) {
+        unsafe {
+            for i in self.pos..N {
+                self.buf[i].assume_init_read(); // take ownership
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,41 @@
 #![no_main]
 #![no_std]
+#![allow(incomplete_features)]
+#![feature(type_alias_impl_trait)]
+#![feature(impl_trait_in_bindings)]
+#![feature(maybe_uninit_uninit_array)]
+#![feature(maybe_uninit_extra)]
+#![feature(maybe_uninit_slice)]
+#![feature(min_type_alias_impl_trait)]
 
-use async_embedded::task;
-use core::time::Duration;
-use cortex_m_rt::{entry, exception, interrupt, ExceptionFrame};
-use embedded_hal::{blocking::delay::DelayMs, prelude::*};
-use nrf52832_hal::{
-    gpio::{
-        self, p0::Parts, Disconnected, Floating, Input, Level, Output, Pin, PullDown, PushPull,
+use core::pin::Pin as StdPin;
+use cortex_m_rt::{entry, exception, ExceptionFrame};
+use embassy::{
+    executor::{task, Executor},
+    time::{Duration, Timer},
+    util::Forever,
+};
+use embassy_nrf::{
+    hal::{
+        clocks,
+        gpio::{
+            self, p0::Parts, Disconnected, Floating, Input, Level, Output, Pin, PullDown, PushPull,
+        },
+        prelude::*,
+        spim::{self, Spim},
+        Delay,
     },
-    pac,
+    interrupt, pac, rtc,
+};
+use embedded_graphics::{
+    pixelcolor::{raw::RawU16, Rgb565, Rgb888},
     prelude::*,
-    spim::{self, Spim},
-    Delay,
 };
 use rtt_target::{rprintln, rtt_init_print};
 
 mod display;
-mod time;
-mod timer;
 
-use display::Display;
+use crate::display::DisplayOff;
 
 #[panic_handler] // panicking behavior
 fn panic(panic_info: &core::panic::PanicInfo) -> ! {
@@ -28,59 +43,81 @@ fn panic(panic_info: &core::panic::PanicInfo) -> ! {
     cortex_m::asm::udf();
 }
 
-type PushPullPin = gpio::Pin<gpio::Output<gpio::PushPull>>;
-
-unsafe fn pre_init() {
-    init_clocks();
-    timer::init();
-}
-
-unsafe fn init_clocks() {
-    // We are exclusive here so can borrow registers safely at most 1 time.
-
-    // configure the LFCLK to use the external crystal (32.768Hz)
-    let clock = &*pac::CLOCK::ptr();
-    clock.lfclksrc.write(|w| w.src().xtal());
-    clock.tasks_lfclkstart.write(|w| w.bits(1));
-
-    // spin until the low freq clock has started up. According to the datasheet this is ~250 μs.
-    // The time could be spend doing something else.
-    while clock.events_lfclkstarted.read().bits() == 0 {
-        continue;
+#[task]
+async fn ms100() {
+    loop {
+        rprintln!("1s tick");
+        Timer::after(Duration::from_millis(1000)).await;
     }
 }
+
+#[task]
+async fn periodic_display(mut d: DisplayOff<pac::SPIM0>) {
+    loop {
+        Timer::after(Duration::from_secs(3)).await;
+        rprintln!("screen power on");
+        let mut don = d.power_on().await;
+        rprintln!("finish: screen power on");
+        for i in 0..100u8 {
+            for j in 0..100u8 {
+                unsafe {
+                    let mut don = StdPin::new_unchecked(&mut don);
+                    let color: Rgb565 = Rgb888::new(i, j, 0).into();
+                    rprintln!("writing pixel {}x{}", i, j);
+                    don.set_pixel(i.into(), j.into(), RawU16::from(color).into_inner())
+                        .await;
+                }
+            }
+        }
+        Timer::after(Duration::from_secs(3)).await;
+        rprintln!("screen power off");
+        d = don.power_off().await;
+        rprintln!("finish: screen power off");
+    }
+}
+
+static RTC: Forever<rtc::RTC<pac::RTC1>> = Forever::new();
+static ALARM: Forever<rtc::Alarm<pac::RTC1>> = Forever::new();
+static EXECUTOR: Forever<Executor> = Forever::new();
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     rprintln!("main");
-    let p = pac::Peripherals::take().unwrap();
-    unsafe { pre_init() }
-    let p0 = Parts::new(p.P0);
-    // *Don't use RTC0!*
 
-    task::spawn(async {
-        loop {
-            rprintln!("100 ms loop");
-            cortex_m::asm::bkpt();
-            timer::wait(Duration::from_millis(100)).await;
-        }
+    let p = pac::Peripherals::take().unwrap();
+
+    clocks::Clocks::new(p.CLOCK)
+        .enable_ext_hfosc()
+        .set_lfclk_src_external(clocks::LfOscConfiguration::NoExternalNoBypass)
+        .start_lfclk();
+
+    let rtc = RTC.put(rtc::RTC::new(p.RTC1, interrupt::take!(RTC1)));
+    rtc.start();
+    unsafe { embassy::time::set_clock(rtc) };
+
+    let p0 = Parts::new(p.P0);
+    let display = DisplayOff::new(
+        p0.p0_22.degrade(),
+        p0.p0_26.degrade(),
+        p0.p0_25.degrade(),
+        p0.p0_18.degrade(),
+        p0.p0_02.degrade(),
+        p0.p0_03.degrade(),
+        p.SPIM0,
+        interrupt::take!(SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0),
+    );
+
+    let alarm = ALARM.put(rtc.alarm0());
+    let executor = EXECUTOR.put(Executor::new());
+    executor.set_alarm(alarm);
+    executor.run(move |spawner| {
+        spawner.spawn(ms100()).unwrap();
+        spawner.spawn(periodic_display(display)).unwrap();
     });
-    //let batt = Battery::new(p0.p0_12);
-    task::block_on(async {
-        loop {
-            rprintln!("1_000 ms loop");
-            //rprintln!("on batt: {}", batt.using_battery());
-            timer::wait(Duration::from_millis(1_000)).await;
-        }
-    });
-    rprintln!("unreachable");
-    // unreachable, but the compiler can't tell.
-    loop {
-        cortex_m::asm::nop();
-    }
 }
 
+/*
 struct Battery {
     pin: gpio::p0::P0_12<Input<PullDown>>,
 }
@@ -104,6 +141,7 @@ impl Battery {
         }
     }
 }
+*/
 
 #[exception]
 fn HardFault(ef: &ExceptionFrame) -> ! {
